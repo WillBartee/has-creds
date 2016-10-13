1322219141d0a8de71dafb5855abbee26f7bd178
willbartee <will.bartee@productops.com>
2016-10-13 21:36:24 +0000

    foo-app test1

--- index.js
+++ index.js
@@ -1,9 +1,35 @@
 var express = require('express');
+var AWS = require('aws-sdk');
+var serviceId = require('service-identity');
 var app = express();
 
 app.get('/', function(req, res) {
-  res.type('text/plain'); // set content-type
-  res.send('i am a beautiful butterfly'); // send text response
+  res.type('application/json'); // set content-type
+  res.send({key:'string'}); // send text response
 });
 
-app.listen(process.env.PORT || 4730);
+app.get('/env', function(req, res) {
+  res.type('application/json'); // set content-type
+  res.send(JSON.stringify(process.env)); // send text response
+});
+
+app.get('/healthcheck', function(req, res) {
+  res.type('application/json'); // set content-type
+  res.send(serviceId.whoami()); // send text response
+});
+
+app.get('/s3get', function(req, res) {
+  var S3 = new AWS.S3();
+  var reqParams = req.query.q
+  var s3params = {
+    Bucket: "pl.degaas.com",
+    Key: reqParams
+  }
+  res.type('application/json');
+  S3.getObject(s3params).
+  on('httpData', function(chunk) { res.write(chunk); }).
+  on('httpDone', function() { res.end(); }).
+  send();
+});
+
+app.listen(process.env.PORT || 8080);
